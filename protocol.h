fee4e56a2ed838971b9797a385bb017b88514d59
Sotiris Blad <sotiris2009@hotmail.com>
2018-08-16 16:18:42 +0300

    Update protocol.h

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 18321 : 13333;
+    return testnet ? 18683 : 29948;
 }
 
 //
